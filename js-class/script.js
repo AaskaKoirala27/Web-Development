32da09bafe712993b1b25ef9c78ec9e050e8e5c1
Aaska Koirala <aaskakoirala@gmail.com>
2025-05-07 10:46:25 +0545

    added js file

---
+++ js-class/script.js
@@ -0,0 +1,35 @@
+// var x = "Aaska Koirala";
+// console.log(x);
+
+// x="Shahrukh Khan";
+// console.log(x);
+
+// let x=5;
+// let y=6;
+
+// let sum = x+y;
+// console.log('sum', sum)
+// let diff = x-y;
+// console.log('diff', diff)
+// let product = x*y;
+// console.log('product', product)
+// let division = x/y;
+// console.log('division', division)
+
+// if (x>y){
+//     console.log(x+' is greater than '+y);
+// }else{
+//     console.log(y+' is greater than '+x);
+// }
+
+//
+
+let year = 2083;
+
+if (year === 2082) {
+    console.log('This is the current year 2082');
+} else if (year < 2082) {
+    console.log('This year has passed.');
+} else {
+    console.log('This year is yet to come.');
+}
